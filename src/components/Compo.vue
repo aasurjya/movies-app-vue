4fe3ce6a39e3f0c394391a5942245bcbd37af9e0
aasurjya <ahandique8@gmail.com>
2023-08-03 15:07:36 +0000

    card and search button

--- src/components/Compo.vue
+++ src/components/Compo.vue
@@ -3,13 +3,19 @@
 </script>
 
 <template>
-  <main class="p-10">
-  <header>
+  <main class="p-10 bg-black">
+      <form>
+        <div class="flex justify-center m-auto">
+          <input type="text" placeholder="Search" class="rounded-md w-2 lg:w-2/12 bg-gray-500 text-white pl-4">
+          <button class="bg-red-500 p-2 rounded-md p- 2 ml-6 text-white">Search</button>
+        </div>
+      </form>
+
     <div class="w-64 bg-purple-300 p-3 rounded-md mt-4">
       <img src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"/>
-
+      <h1>Title</h1>
+      <span>Date</span>
     </div>
-  </header>
   </main>
 </template>
 

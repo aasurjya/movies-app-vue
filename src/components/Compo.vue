e4cda5adb2f0559c1932cce86b005f4332ccc36b
aasurjya <ahandique8@gmail.com>
2023-08-04 14:52:40 +0000

    search feature added

--- src/components/Compo.vue
+++ src/components/Compo.vue
@@ -1,58 +1,84 @@
 <script>
-const URL =  'http://www.omdbapi.com/?s=avengers&apikey=62513876';
-const popURL =  'http://www.omdbapi.com/?s=avengers&apikey=62513876';
-const imgTest = ""
-
 export default {
-  
-    data: ()=> ({
-        imgTest: imgTest,
-        imgs:[]
-    }),
-    mounted(){
-        fetch(popURL).then(
-            (info)=> info.json()
-        ).then(
-            (movies)=>{
-                console.log(movies);
-                this.imgs= movies.Search.reduce((acc, movie)=>{
-                    if(!movie.Poster){
-                        return acc
-                    }
-                    acc.push({
-                        Poster: `${movie.Poster}`,
-                        Title: `${movie.Title}`,
-                        Type: `${movie.Type}`,
-                        Year: `${movie.Year}`,
-                        imdbID: `${movie.imdbID
-}`
-                    })
-                    return acc
-                },[]);
-            }
-        )
-    }
+  data: () => ({
+    imgTest: "",
+    imgs: [],
+    searchTerm: "",
+  }),
+  methods: {
+    search() {
+      if (this.searchTerm === "") {
+        return;
+      }
+      'http://www.omdbapi.com/?s=avengers&apikey=62513876';
 
-}
+      const apiKey = "62513876"; // Replace this with your actual API key
+      const searchURL = `http://www.omdbapi.com/?s=${encodeURIComponent(
+        this.searchTerm
+      )}&apikey=${apiKey}`;
+
+      fetch(searchURL)
+        .then((val) => val.json())
+        .then((movies) => {
+          this.imgs = this.parseImgResponse(movies);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+          this.imgs = []; // Clear the images array in case of an error
+        });
+    },
+    parseImgResponse(movies) {
+      return movies.Search.reduce((acc, movie) => {
+        if (!movie.Poster) {
+          return acc;
+        }
+        acc.push({
+          Poster: `${movie.Poster}`,
+          Title: `${movie.Title}`,
+          Type: `${movie.Type}`,
+          Year: `${movie.Year}`,
+          imdbID: `${movie.imdbID}`,
+        });
+        return acc;
+      }, []);
+    },
+  },
+  mounted() {
+    // Fetch some popular movies on initial load (if required)
+    // This can be removed if you don't want to show popular movies initially.
+    const popURL = 'http://www.omdbapi.com/?s=avengers&apikey=62513876';
+    fetch(popURL)
+      .then((info) => info.json())
+      .then((movies) => {
+        console.log(movies);
+        this.imgs = this.parseImgResponse(movies);
+      });
+  },
+};
 </script>
 
 <template>
-  
   <main class="p-10 bg-black">
-      <form>
-        <div class="flex justify-center m-auto">
-          <input type="text" placeholder="Search" class="rounded-md w-2 lg:w-2/12 bg-gray-500 text-white pl-4">
-          <button class="bg-red-500 p-2 rounded-md p- 2 ml-6 text-white">Search</button>
-        </div>
-      </form>
+    <form @submit.prevent="search">
+      <div class="flex justify-center m-auto">
+        <input
+          v-model="searchTerm"
+          type="text"
+          placeholder="Search"
+          class="rounded-md w-2 lg:w-2/12 bg-gray-500 text-white pl-4"
+        />
+        <button type="submit" class="bg-red-500 p-2 rounded-md p-2 ml-6 text-white">
+          Search
+        </button>
+      </div>
+    </form>
     <div class="flex flex-wrap justify justify-around">
-    <div v-for="img of imgs" :key="img.Title" class="w-64 bg-purple-300 p-3 rounded-md mt-4">
-      <img :src="img.Poster"/>
-      <h1>{{ img.Title }}</h1>
-      <span>{{ img.Year }}</span>
+      <div v-for="img of imgs" :key="img.Title" class="w-64 bg-purple-300 p-3 rounded-md mt-4">
+        <img :src="img.Poster" />
+        <h1>{{ img.Title }}</h1>
+        <span>{{ img.Year }}</span>
       </div>
-      {{ imgs }}
-      </div>
+    </div>
   </main>
 </template>
 
--- src/components/demo.vue
+++ src/components/demo.vue
@@ -1,11 +1,105 @@
-<div v-if="movieData && movieData.length > 0">
-  <div v-for="movie in movieData" :key="movie.imdbID" class="w-64 bg-purple-300 p-3 rounded-md mt-4">
-    <img :src="movie.Poster" alt="Movie Poster" />
-    <h1>{{ movie.Title }}</h1>
-    <span>{{ movie.Year }}</span>
-  </div>
-</div>
+<script>
+const URL =  'http://www.omdbapi.com/?s=avengers&apikey=62513876';
+const popURL =  'http://www.omdbapi.com/?s=avengers&apikey=62513876';
+const imgTest = ""
 
-<div v-else class="w-64 bg-purple-300 p-3 rounded-md mt-4">
-  No movies found.
-</div>
+export default {
+  
+    data: ()=> ({
+        imgTest: imgTest,
+        imgs:[],
+        searchTerm: ""
+    }),
+      methods:{
+        search(){
+          if(this.searchTerm=== ""){
+            return;
+          }
+
+
+          fetch(URL)
+          .then((val)=> val.json())
+          .then((movies)=>{
+             this.imgs = this.parseImgResponse(movies);
+          }
+        )
+      },
+      parseImgResponse(movies){
+        return  movies.Search.reduce((acc, movie)=>{
+                    if(!movie.Poster){
+                        return acc
+                    }
+                    acc.push({
+                        Poster: `${movie.Poster}`,
+                        Title: `${movie.Title}`,
+                        Type: `${movie.Type}`,
+                        Year: `${movie.Year}`,
+                        imdbID: `${movie.imdbID
+}`
+                    })
+                    return acc
+                },[]);
+      }
+          
+        
+        
+      
+      },
+        
+
+
+
+
+
+
+
+
+    mounted(){
+        fetch(popURL).then(
+            (info)=> info.json()
+        ).then(
+            (movies)=>{
+                console.log(movies);
+                this.imgs= movies.Search.reduce((acc, movie)=>{
+                    if(!movie.Poster){
+                        return acc
+                    }
+                    acc.push({
+                        Poster: `${movie.Poster}`,
+                        Title: `${movie.Title}`,
+                        Type: `${movie.Type}`,
+                        Year: `${movie.Year}`,
+                        imdbID: `${movie.imdbID
+}`
+                    })
+                    return acc
+                },[]);
+            }
+        )
+    }
+
+}
+</script>
+
+<template>
+  
+  <main class="p-10 bg-black">
+      <form @submit.prevent="search">
+        <div class="flex justify-center m-auto">
+          <input v-model="searchTerm" type="text" placeholder="Search" class="rounded-md w-2 lg:w-2/12 bg-gray-500 text-white pl-4">
+          <button @click="search" class="bg-red-500 p-2 rounded-md p- 2 ml-6 text-white">Search</button>
+        </div>
+      </form>
+    <div class="flex flex-wrap justify justify-around">
+    <div v-for="img of imgs" :key="img.Title" class="w-64 bg-purple-300 p-3 rounded-md mt-4">
+      <img :src="img.Poster"/>
+      <h1>{{ img.Title }}</h1>
+      <span>{{ img.Year }}</span>
+      </div>
+      {{ imgs }}
+      </div>
+  </main>
+</template>
+
+<style setup>
+</style>
